4a1c39c2048aa340d1d16958e8d64d4e882ef99a
NinaG30 <nina.guemart@gmail.com>
2023-02-06 16:29:59 +0100

    corrigé

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-// Charge le module "express"
+// Charge/import le module "express" pour accéder aux méthodes d'express
 const express = require("express");
-// Charge le module "fs"
+//Charge le module "fs" pour créer et gérer des fichiers 
+//pour y stocker ou lire des fichiers dans le programme Node
+//require inclut le fs à notre projet
 const fs = require("fs");
-// Create a new instance of express
+// Créér une instance d'express
 const app = express();
 
 //Cette fonction gère les demandes GET adressées au chemin "/data"
 app.get("/data", (request, response) => {
+  //lit le fichier data.json
   fs.readFile("data.json", (err, data) => {
     if (err) { //s'il y a une erreur alors il répondra : 
       response.status(500).json({ 
@@ -16,13 +19,14 @@ app.get("/data", (request, response) => {
       });
     } else { //sinon si tout va bien, il répondra :
         //Status 200 est le code pour dire 'tout va bien' dans Express
-      response.status(200).json(JSON.parse(data)); //JSON.parse permet d'avoir a JavaScript object.
+      response.status(200).json(JSON.parse(data)); //JSON.parse permet d'avoir un objet JS (json) 
+      //de l'argument data qui est un string
     }
   });
 });
 
-//"ecoute" le port 3000 
-//The app.listen() method binds itself with the specified host and port to bind and listen for any connections.
+//Expose le port 3000 
+//La fonction se lie avec l'host et le port spécifié
 app.listen(3000, () => {
   console.log("L'application tourne sur le port 3000");
 });
